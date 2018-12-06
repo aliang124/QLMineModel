69d2955f606993b638c09274f34b05ecb0da0e66
计恩良 <454309007@qq.com>
2018-12-06 17:01:12 +0800

    1
    1

--- QLMineModel/QLMineViewController.m
+++ QLMineModel/QLMineViewController.m
@@ -64,41 +64,59 @@
     barView.layer.masksToBounds = YES;
     barView.backgroundColor = [UIColor whiteColor];
     [self.view addSubview:barView];
-
-    
-//    [self initForm];
+    for (int i = 0; i < 3; i++) {
+        [self createItem:i barView:barView];
+    }
+    //下面tableview
+    self.formTable.top = 228+iXStatus;
+    self.formTable.height = WTScreenHeight-WT_TabBar_Height-228-iXStatus;
+    self.formTable.scrollEnabled = NO;
+    [self initForm];
 }
 
+- (void)createItem:(int)idx barView:(UIView *)barView {
+    UIButton *guanBtn = [[UIButton alloc] initWithFrame:CGRectMake((idx*barView.width)/3, 0, barView.width/3, 80)];
+    [barView addSubview:guanBtn];
+    
+    UIImageView *icon1 = [[UIImageView alloc] initWithFrame:CGRectMake((guanBtn.width-24)/2, 20, 24, 24)];
+    icon1.backgroundColor = [UIColor redColor];
+    [guanBtn addSubview:icon1];
+    
+    UILabel *titleLab = [[UILabel alloc] initWithFrame:CGRectMake(0, icon1.bottom+5, guanBtn.width, 11)];
+    titleLab.font = WTFontSys(12);
+    titleLab.text = @"关注";
+    if (idx==1) {
+        titleLab.text = @"粉丝";
+    } else if (idx==2) {
+        titleLab.text = @"消息";
+    }
+    titleLab.textColor = QL_UserName_TitleColor_Black;
+    titleLab.textAlignment = NSTextAlignmentCenter;
+    [guanBtn addSubview:titleLab];
+}
 
 - (void)initForm {
     NSMutableArray *sectionArray = [NSMutableArray array];
     RETableViewSection *section0 = [RETableViewSection section];
     
-    QLMineHeadItem *itHead = [[QLMineHeadItem alloc] init];
-    [section0 addItem:itHead];
-    
     WTIconTextItem *it1 = [[WTIconTextItem alloc] init];
     it1.iconImg = [WTUtil createImageFromColor:[UIColor redColor]];
     it1.textString = @"宝宝档案";
-    it1.hasArrow = YES;
     [section0 addItem:it1];
     
     WTIconTextItem *it2 = [[WTIconTextItem alloc] init];
     it2.iconImg = [WTUtil createImageFromColor:[UIColor redColor]];
     it2.textString = @"关于我们";
-    it2.hasArrow = YES;
     [section0 addItem:it2];
     
     WTIconTextItem *it3 = [[WTIconTextItem alloc] init];
     it3.iconImg = [WTUtil createImageFromColor:[UIColor redColor]];
     it3.textString = @"帮助反馈";
-    it3.hasArrow = YES;
     [section0 addItem:it3];
     
     WTIconTextItem *it4 = [[WTIconTextItem alloc] init];
     it4.iconImg = [WTUtil createImageFromColor:[UIColor redColor]];
     it4.textString = @"设置";
-    it4.hasArrow = YES;
     [section0 addItem:it4];
     
     [sectionArray addObject:section0];
