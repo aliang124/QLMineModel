618ded7ddf895bf37df90cf41d0ceac54bd89b59
计恩良 <454309007@qq.com>
2019-01-09 21:55:01 +0800

    1
    1

--- WTDemo/Pods/QLBusiness/QLBusiness/Util/QLNetWorkingUtil.h
+++ WTDemo/Pods/QLBusiness/QLBusiness/Util/QLNetWorkingUtil.h
@@ -9,4 +9,6 @@
 #import <Foundation/Foundation.h>
 @interface QLNetWorkingUtil : NSObject
 + (void)postDataWithHost:(NSString *)host Path:(NSString *)path Param:(NSDictionary *)param success:(void (^)(id json))success fail:(void (^)(NSString *message))fail;
++ (void)uploadPic:(NSString *)host path:(NSString *)path param:(NSDictionary *)param files:(NSArray *)files success:(void (^)(id json))success fail:(void (^)(NSString *message))fail;
+
 @end
--- WTDemo/Pods/QLBusiness/QLBusiness/Util/QLNetWorkingUtil.m
+++ WTDemo/Pods/QLBusiness/QLBusiness/Util/QLNetWorkingUtil.m
@@ -50,4 +50,50 @@
     }];
 }
 
++ (void)uploadPic:(NSString *)host path:(NSString *)path param:(NSDictionary *)param files:(NSArray *)files success:(void (^)(id json))success fail:(void (^)(NSString *message))fail
+{
+    AFHTTPSessionManager *manager = [AFHTTPSessionManager manager];
+    AFHTTPRequestSerializer *_requestSerializer = [AFHTTPRequestSerializer serializer];
+    [_requestSerializer setTimeoutInterval:30];
+    NSString *auth = @"Bearer";
+    if (![WTUtil strNilOrEmpty:[QLLoginInfo sharedInstance].token]) {
+        auth = [NSString stringWithFormat:@"%@ %@",auth,[QLLoginInfo sharedInstance].token];
+    }
+    [_requestSerializer setValue:auth forHTTPHeaderField:@"Authorization"];
+    [_requestSerializer setValue:@"Ios" forHTTPHeaderField:@"clientType"];
+    manager.requestSerializer = _requestSerializer;
+    
+    AFJSONResponseSerializer *_responseSerializer = [AFJSONResponseSerializer serializer];
+    _responseSerializer.acceptableContentTypes =  [NSSet setWithObjects:@"application/json", @"text/html",@"text/json",@"text/javascript", nil];
+    manager.responseSerializer = _responseSerializer;
+    
+    [manager POST:[NSString stringWithFormat:@"%@%@",host,path] parameters:param constructingBodyWithBlock:^(id<AFMultipartFormData>  _Nonnull formData) {
+        for (int k = 0; k < [files count]; k++) {
+            NSDictionary *dic = [files objectAtIndex:k];
+            NSString *key = [[dic allKeys] objectAtIndex:0];
+            NSData *fileData = [NSData dataWithContentsOfFile:[WTUtil strRelay:dic[key]]];
+            NSString *fileName = [WTUtil strRelay:dic[key]];
+            fileName = [fileName lastPathComponent];
+            if (fileData) {
+                [formData appendPartWithFileData:fileData name:key fileName:[WTUtil strRelay:fileName] mimeType:@"*/*"];
+            }
+        }
+    } progress:^(NSProgress * _Nonnull uploadProgress) {
+    } success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+        if ([[WTUtil strRelay:responseObject[@"resultCode"]] isEqualToString:@"000"]) {
+            if (success) {
+                success(responseObject[@"data"]);
+            }
+        } else {
+            if (fail) {
+                fail([WTUtil strRelay:responseObject[@"message"]]);
+            }
+        }
+    } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+        if (fail) {
+            fail(@"网络错误");
+        }
+    }];
+}
+
 @end
